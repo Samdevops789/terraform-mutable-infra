c2bd46fa2a6b43498a15fe61dd040674635fad7a
Samdevops789 <Sampathbandla2@gmail.com>
2023-01-16 22:14:44 +0530

    updating ./learning-devops7/terraform-mutable-infra

---
+++ outputs.tf
@@ -0,0 +1,28 @@
+output "app_subnets" {
+  value = local.apps_subnets_ids
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "workstation_ip" {
+  value = var.WORKSTATION_IP
+}
+
+output "vpc_cidr" {
+  value = var.vpc_cidr_block
+}
+
+output "public_tg_arn" {
+  value = module.alb.public_tg_arn
+}
+
+output "private_lb_listener_arn" {
+  value = module.alb.private_lb_listener_arn
+}
+
+output "private_lb_dns_name" {
+  value = module.alb.private_lb_dns_name
+}
+Footer
