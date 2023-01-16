baedc8b0ec5a19c22afd7822cb53a04ddc9cd981
Samdevops789 <Sampathbandla2@gmail.com>
2023-01-16 16:43:18 +0530

    updating ./learning-devops7/terraform-mutable-infra

---
+++ vars.tf
@@ -0,0 +1,30 @@
+variable "vpc_cidr_block" {}
+variable "subnets" {}
+variable "env" {}
+variable "AZ" {}
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_route_table_id" {}
+
+
+variable "docdb_engine_version" {}
+variable "docdb_cluster_instances_count" {}
+variable "docdb_instance_class" {}
+
+variable "rds_engine_version" {}
+variable "rds_cluster_instances_count" {}
+variable "rds_instance_class" {}
+
+variable "elasticache_instance_class" {}
+variable "elasticache_cluster_instances_count" {}
+variable "elasticache_engine_version" {}
+
+variable "rabbitmq_instance_type" {}
+
+variable "WORKSTATION_IP" {}
+variable "PUBLIC_DNS_RECORD" {}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
